441fbc149c779e77306281c8a21bb89713704322
Lorenzo-StJohn <britishaccent@bk.ru>
2024-08-14 02:16:12 +0400

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const pair = {};
+  for (let i = 0; i < bracketsConfig.length; ++i) {
+    pair[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+  let st = [];
+  for (let i = 0; i < str.length; ++i) {
+    if (st.length === 0) st.push(str[i]);
+    else {
+      if (pair[st[st.length - 1]] === str[i]) st.pop();
+      else st.push(str[i]);
+    }
+  }
+  return (st.length === 0);
 }
